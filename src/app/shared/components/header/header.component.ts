21c3fb18975aa2927d72f1aef8783048e052f2d8
balsalobre <carlosocb@gmail.com>
2022-01-02 21:15:02 +0100

    Update header styles and adding google font

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { differenceInSeconds, format } from 'date-fns';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  toggleTheme = false;
+  timeSpent: number;
+  timeSpentFormatted = '00:00';
+  loadTime = new Date();
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    setInterval(() => {
+      this.timeSpent = differenceInSeconds(new Date(), this.loadTime);
+      this.timeSpentFormatted = format(new Date(this.timeSpent * 1000), 'mm:ss');
+    }, 1000);
   }
 
+  changeTheme(): void {
+    this.toggleTheme = !this.toggleTheme;
+    if (this.toggleTheme) {
+      this.renderer.addClass(document.body, 'dark');
+    } else {
+      this.renderer.removeClass(document.body, 'dark');
+    }
+  }
+
+  setTimer() {
+    setInterval(() => this.timeSpent = differenceInSeconds(new Date(), this.loadTime), 1000);
+  }
 }
