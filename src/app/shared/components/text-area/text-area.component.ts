a6b454922bb90b0398c827a7a29094de56ba56dc
balsalobre <carlosocb@gmail.com>
2022-01-04 19:34:24 +0100

    stating with text area styles

--- src/app/shared/components/text-area/text-area.component.ts
+++ src/app/shared/components/text-area/text-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-text-area',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./text-area.component.scss']
 })
 export class TextAreaComponent implements OnInit {
+
+  @Input() label: string;
 
   constructor() { }
 
